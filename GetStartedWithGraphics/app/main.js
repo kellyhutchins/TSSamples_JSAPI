23f0152a253ba8cdb9f139f76344ee30c03bc62f
Kelly Hutchins <khutchins@esri.com>
2017-05-31 16:41:20 -0700

    Add getting started with graphics sample

---
+++ GetStartedWithGraphics/app/main.js
@@ -0,0 +1,96 @@
+define(["require", "exports", "esri/Map", "esri/views/MapView", "esri/Graphic", "esri/Color", "esri/geometry/Point", "esri/geometry/Polyline", "esri/geometry/Polygon", "esri/symbols/SimpleMarkerSymbol", "esri/symbols/SimpleLineSymbol", "esri/symbols/SimpleFillSymbol"], function (require, exports, Map, MapView, Graphic, Color, Point, Polyline, Polygon, SimpleMarkerSymbol, SimpleLineSymbol, SimpleFillSymbol) {
+    "use strict";
+    Object.defineProperty(exports, "__esModule", { value: true });
+    var map = new Map({
+        basemap: "hybrid"
+    });
+    var view = new MapView({
+        map: map,
+        container: "viewDiv",
+        center: [-80, 35],
+        zoom: 3
+    });
+    /**********************
+     * Create a point graphic
+     * at the location of the Titanic
+     **********************/
+    var point = new Point({
+        longitude: -49.97,
+        latitude: 41.73
+    });
+    var markerSymbol = new SimpleMarkerSymbol({
+        color: new Color([226, 119, 40]),
+        outline: {
+            color: new Color([255, 255, 255]),
+            width: 2
+        }
+    });
+    var pointGraphic = new Graphic({
+        geometry: point,
+        symbol: markerSymbol
+    });
+    /**********************
+     * Create a polyline graphic
+     * at the location of the Keystone Pipeline
+     **********************/
+    var polyline = new Polyline({
+        paths: [
+            [
+                [-111.30, 52.68],
+                [-98, 49.5],
+                [-93.94, 29.89]
+            ]
+        ]
+    });
+    var lineSymbol = new SimpleLineSymbol({
+        color: new Color([226, 119, 40]),
+        width: 4
+    });
+    var lineAttributes = {
+        Name: "Keystone Pipeline",
+        Owner: "TransCanada",
+        Length: "3,456 km"
+    };
+    var polylineGraphic = new Graphic({
+        geometry: polyline,
+        symbol: lineSymbol,
+        attributes: lineAttributes,
+        popupTemplate: {
+            title: "{Name}",
+            content: [{
+                    type: "fields",
+                    fieldInfos: [{
+                            fieldName: "Name"
+                        }, {
+                            fieldName: "Owner"
+                        }, {
+                            fieldName: "Length"
+                        }]
+                }]
+        }
+    });
+    /****************
+     * Create a polygon graphic
+     * **************** */
+    var polygon = new Polygon({
+        rings: [[
+                [-64.78, 32.3],
+                [-66.07, 18.45],
+                [-80.21, 25.78],
+                [-64.78, 32.3]
+            ]]
+    });
+    var fillSymbol = new SimpleFillSymbol({
+        color: new Color([227, 139, 79, 0.8]),
+        outline: {
+            color: new Color([255, 255, 255]),
+            width: 1
+        }
+    });
+    var polygonGraphic = new Graphic({
+        geometry: polygon,
+        symbol: fillSymbol
+    });
+    view.graphics.addMany([pointGraphic, polylineGraphic, polygonGraphic]);
+});
+//# sourceMappingURL=main.js.map
---
+++ GetStartedWithGraphics/app/main.ts
@@ -0,0 +1,121 @@
+import Map = require("esri/Map");
+import MapView = require("esri/views/MapView");
+import Graphic = require("esri/Graphic");
+import Color = require("esri/Color");
+
+import Point = require("esri/geometry/Point");
+import Polyline = require("esri/geometry/Polyline");
+import Polygon = require("esri/geometry/Polygon");
+
+import SimpleMarkerSymbol = require("esri/symbols/SimpleMarkerSymbol");
+import SimpleLineSymbol = require("esri/symbols/SimpleLineSymbol");
+import SimpleFillSymbol = require("esri/symbols/SimpleFillSymbol");
+
+import esri = __esri;
+
+const map = new Map({
+    basemap: "hybrid"
+});
+const view = new MapView({
+    map: map,
+    container: "viewDiv",
+    center: [-80, 35],
+    zoom: 3
+});
+
+/**********************
+ * Create a point graphic
+ * at the location of the Titanic 
+ **********************/
+
+
+const point = new Point({
+    longitude: -49.97,
+    latitude: 41.73
+});
+
+const markerSymbol = new SimpleMarkerSymbol({
+    color: new Color([226, 119, 40]),
+    outline: {
+        color: new Color([255, 255, 255]),
+        width: 2
+    }
+});
+
+const pointGraphic = new Graphic({
+    geometry: point,
+    symbol: markerSymbol
+});
+
+
+/**********************
+ * Create a polyline graphic
+ * at the location of the Keystone Pipeline
+ **********************/
+const polyline = new Polyline({
+    paths: [
+        [
+            [-111.30, 52.68],
+            [-98, 49.5],
+            [-93.94, 29.89]
+        ]
+    ]
+});
+
+const lineSymbol = new SimpleLineSymbol({
+    color: new Color([226, 119, 40]),
+    width: 4
+});
+
+const lineAttributes = {
+    Name: "Keystone Pipeline",
+    Owner: "TransCanada",
+    Length: "3,456 km"
+};
+
+const polylineGraphic = new Graphic({
+    geometry: polyline,
+    symbol: lineSymbol,
+    attributes: lineAttributes,
+    popupTemplate: {
+        title: "{Name}",
+        content: [{
+            type: "fields",
+            fieldInfos: [{
+                fieldName: "Name"
+            }, {
+                fieldName: "Owner"
+            }, {
+                fieldName: "Length"
+            }]
+        }]
+    }
+});
+
+/****************
+ * Create a polygon graphic
+ * **************** */
+
+const polygon = new Polygon({
+    rings: [[
+        [-64.78, 32.3],
+        [-66.07, 18.45],
+        [-80.21, 25.78],
+        [-64.78, 32.3]
+    ]]
+});
+
+const fillSymbol = new SimpleFillSymbol({
+    color: new Color([227, 139, 79, 0.8]),
+    outline: {
+        color: new Color([255, 255, 255]),
+        width: 1
+    }
+});
+
+const polygonGraphic = new Graphic({
+    geometry: polygon,
+    symbol: fillSymbol
+});
+
+view.graphics.addMany([pointGraphic, polylineGraphic, polygonGraphic]);
